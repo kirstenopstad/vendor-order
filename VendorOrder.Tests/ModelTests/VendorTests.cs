1138c721b7f348975ebef3081ca616f953bd532e
Kirsten Opstad <kirsten.opstad@gmail.com>
2022-12-16 09:01:55 -0800

    add tests for vendor class

--- VendorOrder.Tests/ModelTests/VendorTests.cs
+++ VendorOrder.Tests/ModelTests/VendorTests.cs
@@ -1,16 +1,100 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using VendorOrder.Models;
+using System.Collections.Generic;
+using System;
 
 namespace VendorOrder.Tests
 {
   [TestClass]
-  public class VendorTests
+  public class VendorTests : IDisposable
   {
-    [TestMethod]
-    public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
+    public void Dispose()
     {
-      // any necessary logic to prep for test; instantiating new classes, etc.
-      // Template: ``` Assert.AreEqual(EXPECTED_RESULT, CODE_TO_TEST); ```
+      Vendor.ClearAll();
+    }
+
+    [TestMethod]
+    public void CreateInstance_CreateInstanceOfVendor_Vendor()
+    {
+      string name="Suzie's Cafe";
+      string description="Coffee shop";
+      Vendor newVendor = new Vendor(name, description);
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+
+    [TestMethod]
+    public void GetName_GetVendorInstanceName_String()
+    {
+      // Arrange
+      string name="Suzie's Cafe";
+      string description="Coffee shop";
+      Vendor newVendor = new Vendor(name, description);
+      //Act
+      string result = newVendor.Name;
+      //Assert
+      Assert.AreEqual(name, result);
+    }
+
+    [TestMethod]
+    public void GetDescription_GetVendorInstanceDescription_String()
+    {
+      // Arrange
+      string name="Suzie's Cafe";
+      string description="Coffee shop";
+      Vendor newVendor = new Vendor(name, description);
+      //Act
+      string result = newVendor.Description;
+      //Assert
+      Assert.AreEqual(name, result);
+    }
+
+    [TestMethod]
+    public void GetEmptyList_GetEmptyListOfVendorOrders_List()
+    {
+      // Arrange
+      string name="Suzie's Cafe";
+      string description="Coffee shop";
+      Vendor newVendor = new Vendor(name, description);
+      List<Vendor> newList = new List<Vendor> {};
+      // Act
+      List<Vendor> result = Vendor.Orders;
+      // Assert
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+    [TestMethod]
+    public void GetAll_GetAllInstancesOfVendor_List<Vendor>()
+    {
+      // Arrange
+      string name="Suzie's Cafe";
+      string description="Coffee shop";
+      Vendor newVendor1 = new Vendor(name, description);
+      string name="Happy Returns";
+      string description="Retirement home";
+      Vendor newVendor2 = new Vendor(name, description);
+      List<Vendor> newList = new List<Vendor> { newVendor1, newVendor2 };
+      // Act
+      List<Vendor> result = Vendor.GetAll();
+      // Assert
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+    [TestMethod]
+    public void ClearAll_ClearAllInstancesOfVendor_List<Vendor>()
+    {
+      // Arrange
+      string name="Suzie's Cafe";
+      string description="Coffee shop";
+      Vendor newVendor1 = new Vendor(name, description);
+      string name="Happy Returns";
+      string description="Retirement home";
+      Vendor newVendor2 = new Vendor(name, description);
+      List<Vendor> newList = new List<Vendor> {};
+      // Act
+      Vendor.ClearAll();
+      List<Vendor> result = Vendor.GetAll();
+      // Assert
+      CollectionAssert.AreEqual(newList, result); 
     }
   }
 }
