d2122a7386153aebce63e5dd3375ce6ad00f0499
Kirsten Opstad <kirsten.opstad@gmail.com>
2022-12-16 09:21:33 -0800

    passes tests GetAllEmpty & ClearAll

--- VendorOrder/Models/Vendor.cs
+++ VendorOrder/Models/Vendor.cs
@@ -7,7 +7,7 @@ namespace VendorOrder.Models
     // properties
     public string Name { get; set; }
     public string Description { get; set; }
-    private List<Vendor> _instances = new List<Vendor> {};
+    private static List<Vendor> _instances = new List<Vendor> {};
 
     // constructor 
     public Vendor(string name, string description)
@@ -17,6 +17,15 @@ namespace VendorOrder.Models
       _instances.Add(this);
     }
     // methods
+    public static List<Vendor> GetAll()
+    {
+      return _instances;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
     
   }
 }
