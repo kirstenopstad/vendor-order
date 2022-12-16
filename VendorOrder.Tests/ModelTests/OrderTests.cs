811cd3dfc6820857bcb831346f683a99344d5b7d
Kirsten Opstad <kirsten.opstad@gmail.com>
2022-12-16 09:58:05 -0800

    add tests for create instance & get title, desc, price, date & id

--- VendorOrder.Tests/ModelTests/OrderTests.cs
+++ VendorOrder.Tests/ModelTests/OrderTests.cs
@@ -1,5 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using VendorOrder.Models;
+using System.Collections.Generic;
+using System;
 
 namespace VendorOrder.Tests
 {
@@ -7,11 +9,89 @@ namespace VendorOrder.Tests
   public class OrderTests
   {
     [TestMethod]
-    // Test methods will go here
-    public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
+    public void CreateInstance_CreateNewInstanceOfOrder_Order()
     {
-      // any necessary logic to prep for test; instantiating new classes, etc.
-      // Template: ``` Assert.AreEqual(EXPECTED_RESULT, CODE_TO_TEST); ```
+      string title = "Weekend";
+      string description = "Four dozen croissants";
+      int price = 40;
+      string date = "December 16, 2022";
+      Order newOrder = new Order(title, description, price, date);
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+
+    [TestMethod]
+    public void GetTitle_GetInstanceTitle_string()
+    {
+      // Arrange
+      string title = "Weekend";
+      string description = "Four dozen croissants";
+      int price = 40;
+      string date = "December 16, 2022";
+      Order newOrder = new Order(title, description, price, date);
+      // Act
+      string result = newOrder.Title;
+      // Assert
+      Assert.AreEqual(title, result);
+    }
+
+    [TestMethod]
+    public void GetDescription_GetInstanceDescription_string()
+    {
+      // Arrange
+      string title = "Weekend";
+      string description = "Four dozen croissants";
+      int price = 40;
+      string date = "December 16, 2022";
+      Order newOrder = new Order(title, description, price, date);
+      // Act
+      string result = newOrder.Description;
+      // Assert
+      Assert.AreEqual(description, result);
+    }
+
+    [TestMethod]
+    public void GetPrice_GetInstancePrice_string()
+    {
+      // Arrange
+      string title = "Weekend";
+      string description = "Four dozen croissants";
+      int price = 40;
+      string date = "December 16, 2022";
+      Order newOrder = new Order(title, description, price, date);
+      // Act
+      int result = newOrder.Price;
+      // Assert
+      Assert.AreEqual(price, result);
+    }
+
+    [TestMethod]
+    public void GetDate_GetInstanceDate_string()
+    {
+      // Arrange
+      string title = "Weekend";
+      string description = "Four dozen croissants";
+      int price = 40;
+      string date = "December 16, 2022";
+      Order newOrder = new Order(title, description, price, date);
+      // Act
+      string result = newOrder.Date;
+      // Assert
+      Assert.AreEqual(date, result);
+    }
+
+    [TestMethod]
+    public void GetId_GetInstanceId_string()
+    {
+      // Arrange
+      string title = "Weekend";
+      string description = "Four dozen croissants";
+      int price = 40;
+      string date = "December 16, 2022";
+      Order newOrder = new Order(title, description, price, date);
+      // Act
+      int result = newOrder.Id;
+      // Assert
+      Assert.AreEqual(1, result);
     }
   }
 }
