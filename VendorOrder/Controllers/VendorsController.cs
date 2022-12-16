534dfe54f7350c66199fe48819f6b0f9b4a1877d
Kirsten Opstad <kirsten.opstad@gmail.com>
2022-12-16 11:21:38 -0800

    update order instance list to empty until Order Create() route exists

--- VendorOrder/Controllers/VendorsController.cs
+++ VendorOrder/Controllers/VendorsController.cs
@@ -27,5 +27,21 @@ namespace VendorOrder.Controllers
       Vendor newVendor = new Vendor(name, description);
       return RedirectToAction("Index");
     }
+
+    [HttpGet("/vendors/{vendorId}")]
+    public ActionResult Show(int vendorId)
+    {
+      // Create dictionary to return multiple objects (vendor and orders)
+      Dictionary<string, object> model = new Dictionary<string, object>() {};
+      // Get vendor obj
+      Vendor foundVendor = Vendor.Find(vendorId);
+      // TODO: Get list of instances of orders relating to this vendor [GetAll()]
+      List<Order> vendorOrders = new List<Order> {};
+      // Add to dictionary
+      model.Add("vendor", foundVendor);
+      model.Add("orders", vendorOrders);
+      // Return Show and pass in model
+      return View("Show", model);
+    }
   }
 }
