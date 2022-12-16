af5eb8c825c0d1314efbe150a0d0f8b1cf1287ad
Kirsten Opstad <kirsten.opstad@gmail.com>
2022-12-16 14:43:51 -0800

    add Show() route

--- VendorOrder/Controllers/OrdersController.cs
+++ VendorOrder/Controllers/OrdersController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using VendorOrder.Models;
+using System.Collections.Generic;
 
 namespace VendorOrder.Controllers
 {
@@ -10,6 +11,22 @@ namespace VendorOrder.Controllers
     { 
       Vendor selectedVendor = Vendor.Find(vendorId);
       return View(selectedVendor); 
+    }
+
+    // Finds single order by id
+    [HttpGet("/vendors/{vendorId}/orders/{orderId}")]
+    public ActionResult Show(int vendorId, int orderId)
+    {
+      // Create dict to hold both vendor and order
+      Dictionary<string, object> model = new Dictionary<string, object>() {};
+      // Get Vendor, get Order
+      Vendor foundVendor = Vendor.Find(vendorId);
+      Order foundOrder = Order.Find(orderId);
+      // Add to dict
+      model.Add("vendor", foundVendor);
+      model.Add("orders", foundOrder);
+      // Return view Show() and pass in model
+      return View("Show", model);
     }
   }
 }
